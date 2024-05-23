b5de9e0a53ab406e423cf46001081c5b85af63f7
luana <luana@DESKTOP-TIL0PLS>
2024-05-23 12:16:45 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD.SQL/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.SQL.Models
+{
+    public class Context : DbContext
+    {
+        public Context( DbContextOptions<Context> options ):base(options)
+        {
+
+        }     
+        
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ CRUD.SQL/Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿using OneOf.Types;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD.SQL.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name ="Codigo")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("Valor")]
+        [Display(Name = "Valor")]
+        public float Valor { get; set; }
+
+        [Column("Estoque")]
+        [Display(Name = "Estoque")]
+        public int Estoque { get; set; }
+    }
+}
